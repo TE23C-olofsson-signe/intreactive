5574cece66270c751bc59d0dcbeffbcd936382f3
Signe Olofsson <signe.olofsson@elev.ga.ntig.se>
2024-08-30 14:50:18 +0200

    lop

--- interaktiv/Program.cs
+++ interaktiv/Program.cs
@@ -1,2 +1,13 @@
 ﻿Console.WriteLine("Användarnamn:");
 string name = Console.ReadLine();
+
+if (name == "Signe")
+{
+    Console.WriteLine("välkomen!");
+}
+else 
+{
+    Console.WriteLine(" Vem är du fanta");
+}
+
+Console.ReadLine(); 
